452babeebd9222a6fa6dde3a3bacec47743e622f
heyjc@yonyou.com <18224487974@163.com>
2018-04-11 11:08:25 +0800

    download

--- src/wechat/download.vue
+++ src/wechat/download.vue
@@ -6,21 +6,27 @@
 
 <script>
 import { downloadFileForUrl } from 'hyj-func'
+import { Base64 } from 'js-base64'
 
 export default {
   name: 'Download',
   components: {},
   data () {
     return {
-      msg: navigator.userAgent.indexOf('MicroMessenger')>-1?'微信不支持下载，请点击右上角，选择在浏览器中打开下载':'',
+      msg: navigator.userAgent.indexOf('MicroMessenger')>-1?'微信不支持下载，请点击右上角，选择在浏览器中打开下载':''
     }
   },
-  methods: {},
+  methods: {
+    b64DecodeUnicode: function(str) {
+      return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+      }).join(''))
+    }
+  },
   mounted: function(){
     if (navigator.userAgent.indexOf('MicroMessenger') === -1) {
-      console.log(document.location.search.slice(1).split('=')[1])
-      const downloadUrl = window.btoa(document.location.search.slice(1).split('=')[1])
-      downloadFileForUrl(downloadUrl)
+      const downloadUrl = document.location.search.slice(1).split('=')[1]
+      downloadFileForUrl(this.b64DecodeUnicode(downloadUrl))
     }
   }
 }
